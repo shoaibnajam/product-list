38deff1a3d5b4b2ae4a5963512c0dce5ee86c949
Shoaib Najam <shoaibnajam@Talals-MacBook-Pro.local>
2024-01-12 05:50:38 +0000

    changed sorting and styles

--- src/App.js
+++ src/App.js
@@ -1,28 +1,86 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './App.css';
-import { useState, useEffect, useRef } from 'react';
+
+const dummyData = [
+  {
+    id: 1000,
+    product: 'Tomatoes',
+    useByDate: '2024-10-03',
+  },
+  {
+    id: 2000,
+    product: 'Bread',
+    useByDate: '2024-05-05',
+  },
+  {
+    id: 3000,
+    product: 'Butter',
+    useByDate: '2024-03-24',
+  },
+  {
+    id: 4000,
+    product: 'Aubergine',
+    useByDate: '2024-08-07',
+  },
+]
+
+const today = new Date().toISOString().split('T')[0];
+
+const validateString = (input) => {
+  return input && typeof input === 'string' && input.trim() !== ""
+};
+
+const formatDate = (dateString) => {
+  if (!validateString(dateString)) {
+    return today;
+  } 
+  return dateString.split('-').reverse().join('-');
+}
 
 let productSortOrder = false;
 let dateSortOrder = false;
 
+
+const populateDummyData = (data) => {
+  window.localStorage.setItem('data', JSON.stringify(data));
+}
+populateDummyData(dummyData);
+
+const initialLocalStorageData = JSON.parse(window.localStorage.getItem('data'));
+
 const App = () => {
   const [product, setProduct] = useState('');
-  const [useByDate, setUseByDate] = useState('');
-  const [data, setData] = useState([]);
+  const [useByDate, setUseByDate] = useState(today);
+  const [data, setData] = useState(initialLocalStorageData);
   const [id, setId] = useState(1);
-  const inputRef = useRef(null)
+  const inputRef = useRef(null);
+  
   useEffect(() => {
-  })
+    // Update data in localStorage
+    window.localStorage.setItem('data', JSON.stringify(data));
+  }, [data]);
 
   const handleSubmit = e => {
     e.preventDefault();
+    
+    // Perform validation
+    if (!validateString(product) || !validateString(useByDate)) {
+      inputRef.current.focus()
+      return;
+    }
+  
+    // Update data state 
     setData((row) => [
       ...row,
       { id: id, product: product, useByDate: useByDate }
     ])
+    
+    // Increment guid
     setId(prevValue => {
       return prevValue + 1;
     });
+
+    // Focus on and clear input for next entry
     inputRef.current.focus()
     setProduct('')
   }
@@ -36,32 +94,40 @@ const App = () => {
   }
 
   const sortProduct = () => {
+    // Toggle sorting order
     productSortOrder = !productSortOrder
-    const unsortedArray = [...data];
-    const sortedArr = unsortedArray.sort(compareFunction);
+
     function compareFunction(a, b) {
       return productSortOrder ?
         a.product.localeCompare(b.product) :
         b.product.localeCompare(a.product);
     }
+
+    // Make a copy of state, sort it and perform setState
+    const unsortedArray = [...data];
+    const sortedArr = unsortedArray.sort(compareFunction);
     setData(sortedArr)
   }
 
   const sortUseByDate = () => {
+    // Toggle sorting order
     dateSortOrder = !dateSortOrder
-    const unsortedArray = [...data];
-    const sortedArr = unsortedArray.sort(compareFunction);
+    
     function compareFunction(a, b) {
       return dateSortOrder ?
         new Date(a.useByDate) - new Date(b.useByDate) :
         new Date(b.useByDate) - new Date(a.useByDate);
     }
+    
+    // Make a copy of state, sort it and perform setState
+    const unsortedArray = [...data];
+    const sortedArr = unsortedArray.sort(compareFunction);
     setData(sortedArr)
   }
 
   const NoDataContainer = () => {
     return (
-      <div class="no-data-container">Sorry - no data</div>
+      <div class="no-data-container">No data available</div>
     )
   }
 
@@ -70,8 +136,10 @@ const App = () => {
       <div className='form-container'>
         <h1>Enter product</h1>
         <form id="product-form" onSubmit={handleSubmit}>
-          <input type="text" placeholder="Enter Product..." value={product} onChange={e => setProduct(e.target.value)} ref={inputRef} />
-          <input type="date" value={useByDate} onChange={e => setUseByDate(e.target.value)} />
+          <div className="input-group">
+            <input type="text" placeholder="Enter Product..." value={product} onChange={e => setProduct(e.target.value)} ref={inputRef} />
+            <input type="date" min={today} value={useByDate} onChange={e => setUseByDate(e.target.value)} />
+          </div>
           <button className='btn addBtn' type="submit"  >Add</button>
         </form>
       </div>
@@ -79,14 +147,13 @@ const App = () => {
       <div className='rowHeader' ><p>Sort by: <button className='btn btn-small' onClick={sortProduct}>Product Name</button><button className='btn btn-small' onClick={sortUseByDate}>Use by Date</button></p></div>
 
       <div className='container'>
-        {data.length > 0 ? data.map(
+        {(data && data.length > 0) ? data.map(
           (row, index) => {
-            console.log('hello');
             return (
-              <div className='rowWrapper'>
+              <div key={row.id} className='rowWrapper'>
                 <div className='row'>
-                  <p><span > {index + 1} : <strong>{row.product}</strong></span></p>
-                  <p><span> <strong>{row.useByDate}</strong></span> </p>
+                  <p>{index + 1}- Name: <strong>{row.product}</strong></p>
+                  <p>Best before: <strong>{formatDate(row.useByDate)}</strong></p>
                 </div>
                 <button className='btn deleteBtn' onClick={(e) => removeProduct(e, row.id)} >Delete</button>
               </div>
@@ -94,16 +161,8 @@ const App = () => {
           }
         ) : (<NoDataContainer />)}
       </div>
-
-
     </div>
-
   );
-
-
-
 }
+
 export default App;
-
-
-
